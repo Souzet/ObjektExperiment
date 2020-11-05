06a14e2928701ce7257dbe55d2545ea9f65bab84
souzet tadros <souzettadros@gmail.com>
2020-11-05 19:51:32 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjektExperiment
+{
+    class Program
+    {
+        static double Sqr(double X)
+        {
+            return X * X;
+        }
+        class Person
+        {
+            private string name, phone;  
+            public Person( string N, string P)
+            {
+                name = N; phone = P;
+            }
+            public string NamePhone()
+            {
+                return $"{name}, tel. {phone}";
+
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            // ------skyddad objektlösning------
+            Person P = new Person("Arne Anka", "013 - 13 13 13");
+            Person Q = new Person("Anna Anka", "013-13 16 19");
+
+            Console.WriteLine(P.NamePhone());
+            Console.WriteLine(Q.NamePhone());
+
+
+        }
+    }
+}
