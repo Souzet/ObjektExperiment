bda99cf0a14e46606a557e3c9e9acc4aadf3d0ab
souzet tadros <souzettadros@gmail.com>
2020-11-05 20:09:54 +0100

    1

--- Program.cs
+++ Program.cs
@@ -8,32 +8,27 @@ namespace ObjektExperiment
 {
     class Program
     {
-        static double Sqr(double X)
-        {
-            return X * X;
-        }
+        
         class Person
         {
-            private string name, phone;  
-            public Person( string N, string P)
-            {
-                name = N; phone = P;
-            }
-            public string NamePhone()
-            {
-                return $"{name}, tel. {phone}";
-
-            }
+            public string förnamn, efternamn;                         
         }
         static void Main(string[] args)
         {
+            //string förnamn, efternamn;
+            Person P = new Person()
+            {
+                förnamn = "Kalle",
+                efternamn = "Anka"
+            };
+            Person Q = new Person()
+            {            
+                förnamn = "Kajsa",
+                efternamn = "Anka"
+            };
+            
 
-            // ------skyddad objektlösning------
-            Person P = new Person("Arne Anka", "013 - 13 13 13");
-            Person Q = new Person("Anna Anka", "013-13 16 19");
-
-            Console.WriteLine(P.NamePhone());
-            Console.WriteLine(Q.NamePhone());
+            
 
 
         }
